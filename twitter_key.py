583d342fb10f50861ecf0a986910e62d950bf9df
scott <scottmooney1111@yahoo.com>
2015-11-06 09:55:10 +0000

    twittwer

---
+++ twitter_key.py
@@ -0,0 +1,13 @@
+CONSUMER_KEY =''
+
+CONSUMER_SECRET =''
+
+ACCESS_TOKEN =''
+
+ACCESS-TOKEN_SECRET = ''
+
+auth = tweepy.OAuthhandler(CONSUMER_KEY, CONSUMER_SECRET)
+
+auth.set_access_token(ACCESS-TOKEN, ACCESS_TOKEN_SECRET)
+
+twitter = tweepy.API(auth)
